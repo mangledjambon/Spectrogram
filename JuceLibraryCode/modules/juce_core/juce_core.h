a5ae9310ea6834933ed11603b7a3914b426bf018
Sean Breen <seanbreen91@gmail.com>
2015-12-01 01:15:54 +0000

    added spectrogram component

---
+++ JuceLibraryCode/modules/juce_core/juce_core.h
@@ -0,0 +1,12 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#if defined (JUCER_VS2015_78A5022)
+ #include "D:\Sean\Cpp\JUCE\modules\juce_core\juce_core.h"
+#elif defined (JUCER_VS2010_78A501D)
+ #include "D:\Sean\Cpp\JUCE\modules\juce_core\juce_core.h"
+#elif defined (JUCER_LINUX_MAKE_6D53C8B4)
+ #include "D:/Sean/Cpp/JUCE/modules/juce_core/juce_core.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
